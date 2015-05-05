31f7f22ca5a2275694fe894d0b8a96d3220f9ed0
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2015-05-05 23:11:40 +0900

    Solve small input

--- workspace/main.py
+++ workspace/main.py
@@ -1,19 +1,47 @@
+def primes(n):
+    res = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            res.append(d)
+            n /= d
+        d += 1
+    if n > 1:
+        res.append(n)
+    return res
+
 def calc(a):
-    x = [s.rfind('1') for s in a]
-    res = 0
-    for i in range(len(x)):
-        for j in range(i, len(x)):
-            if x[j] <= i:
-                for k in range(j, i, -1):
-                    x[k], x[k - 1] = x[k - 1], x[k]
-                res += j - i
+    twos = 0
+    threes = 0
+    fives = 0
+    for k in a:
+        p = primes(k)
+        twos = max(twos, p.count(2))
+        threes = max(threes, p.count(3))
+        fives = max(fives, p.count(5))
+    res = []
+    res += [5] * fives
+    res += [3] * threes
+    res += [4] * (twos / 2)
+    res += [2] * (twos % 2)
+    if len(res) == 3:
+        return res
+
+    while len(res) < 3:
+        for i in range(2, 6):
+            if not i in res:
+                res.append(i)
                 break
     return res
 
 def result():
-    return calc([raw_input() for x in range(int(raw_input()))])
+    R, N, M, K = [int(x) for x in raw_input().split()]
+    for r in range(R):
+        products = [int(k) for k in raw_input().split()]
+        print ''.join([str(i) for i in calc(products)])
 
 if __name__ == "__main__":
     T = int(raw_input())
     for t in range(1, T + 1):
-        print "Case #{0}: {1}".format(t, result())
+        print "Case #{0}:".format(t)
+    result()
--- workspace/test.py
+++ workspace/test.py
@@ -3,9 +3,9 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(calc(["10", "11"]), 0)
+        self.assertEqual(calc([9, 4, 36, 1]), [3, 3, 4])
     def test_case2(self):
-        self.assertEqual(calc(["001", "100", "010"]), 2)
+        self.assertEqual(calc([1, 1, 1, 1]), [2, 3, 4])
 
 if __name__ == '__main__':
     unittest.main()
