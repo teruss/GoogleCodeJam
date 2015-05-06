6b1439d11f18512b3dcc3e9452d259d78a54e169
Teruyoshi Sasaki <teruyoshi.sasaki@gmail.com>
2015-05-06 21:30:32 +0900

    Solve C-small-2

--- workspace/main.py
+++ workspace/main.py
@@ -1,44 +1,57 @@
-def primes(n):
-    res = []
-    d = 2
-    while d * d <= n:
-        while n % d == 0:
-            res.append(d)
-            n /= d
-        d += 1
-    if n > 1:
-        res.append(n)
-    return res
+import math
+import os.path
 
-def calc(a):
-    twos = 0
-    threes = 0
-    fives = 0
-    for k in a:
-        p = primes(k)
-        twos = max(twos, p.count(2))
-        threes = max(threes, p.count(3))
-        fives = max(fives, p.count(5))
-    res = []
-    res += [5] * fives
-    res += [3] * threes
-    res += [4] * (twos / 2)
-    res += [2] * (twos % 2)
-    if len(res) == 3:
+def createP(A):
+    def createB(a):
+        m = {}
+        for y in range(1 << len(a)):
+            l = 1
+            for k in range(len(a)):
+                l *= a[k] if (y & (1 << k)) != 0 else 1
+            if l not in m:
+                m[l] = 0
+            m[l] += 1
+        return m
+    return [createB(a) for a in A]
+
+def createA(A, c, N, M):
+    if len(c) == N:
+        A.append(list(c))
+        return
+    for x in range(2 if len(c) == 0 else c[-1], M + 1):
+        c.append(x)
+        createA(A, c, N, M)
+        del c[-1]
+
+def Pr(N, M, A):
+    nf = math.factorial(N)
+    mn = math.pow(M - 1, N)
+    k = nf / mn
+    def count_factorial(x):
+        res = 1
+        for i in range(2, M + 1):
+            res *= math.factorial(x.count(i))
         return res
+    return [k / count_factorial(a) for a in A]
 
-    while len(res) < 3:
-        for i in range(2, 6):
-            if not i in res:
-                res.append(i)
-                break
-    return res
+def update(PrA, p, k):
+    return [pra * (pp[k] if k in pp else 0) for (pra, pp) in zip(PrA, p)]
+        
+def calc(N, M, a, A, p, PrA):
+    for k in a:
+        PrA = update(PrA, p, k)
+    return A[PrA.index(max(PrA))]
 
 def result():
     R, N, M, K = [int(x) for x in raw_input().split()]
+    A = []
+    createA(A, [], N, M)
+    PrA = Pr(N, M, A)
+    p = createP(A)        
+
     for r in range(R):
         products = [int(k) for k in raw_input().split()]
-        print ''.join([str(i) for i in calc(products)])
+        print ''.join([str(i) for i in calc(N, M, products, A, p, list(PrA))])
 
 if __name__ == "__main__":
     T = int(raw_input())
--- workspace/test.py
+++ workspace/test.py
@@ -3,9 +3,9 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_case1(self):
-        self.assertEqual(calc([9, 4, 36, 1]), [3, 3, 4])
+        self.assertEqual(calc(3, 5, [9, 4, 36, 1]), [3, 3, 4])
     def test_case2(self):
-        self.assertEqual(calc([1, 1, 1, 1]), [2, 3, 4])
+        self.assertEqual(calc(3, 5, [1, 1, 1, 1]), [2, 3, 4])
 
 if __name__ == '__main__':
     unittest.main()
